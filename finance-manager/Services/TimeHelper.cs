4b9dbeb2218126032d330fcd340a411eefde3f91
b0c-beep <bocmarioadrian98@gmail.com>
2025-03-22 20:21:09 +0200

    Solid progress on dashboard page

---
+++ finance-manager/Services/TimeHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finance_manager.Services
+{
+    class TimeHelper
+    {
+        private static readonly string logPath = "./lastLog.txt";
+
+        public static void checkFile()
+        {
+            if (!System.IO.File.Exists(logPath))
+            {
+                System.IO.File.Create(logPath).Close();
+            }
+        }
+
+        public static string getCurrentDate()
+        {
+
+            string date = DateTime.Now.Month + "/" + DateTime.Now.Year;
+            return date;
+        }
+
+        public static void logDate()
+        {
+            string date = getCurrentDate();
+            System.IO.File.WriteAllText(logPath, string.Empty);
+            System.IO.File.WriteAllText(logPath, date);
+        }
+
+        public static string getLastDate()
+        {
+            return System.IO.File.Exists(logPath) ? System.IO.File.ReadAllText(logPath).Trim() : "";
+        }
+
+        public static bool didMonthPass()
+        {
+            string lastDate = getLastDate();
+            string currentDate = getCurrentDate();
+            if (lastDate != currentDate)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- finance-manager/Views/DashboardPage.xaml.cs
+++ finance-manager/Views/DashboardPage.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using finance_manager.Services;
+using finance_manager.Models;
+using System.Data.Entity;
+using finance_manager.Data; 
 
 namespace finance_manager.Views
 {
@@ -23,6 +27,78 @@ namespace finance_manager.Views
         public DashboardPage()
         {
             InitializeComponent();
+            TimeHelper.checkFile();
+            if (TimeHelper.didMonthPass())
+            {
+                resetMonth();
+            }
+            TimeHelper.logDate();
+            printDate();
+            loadProfitOptions(DatabaseHelper.FetchAllProducts());
+            loadCostOptions(DatabaseHelper.FetchAllExpenses());
+        }
+
+        private void printDate()
+        { 
+            dateLabel.Content = "Current situation for " + TimeHelper.getCurrentDate();
+            dateLabel.FontSize = 18;
+            dateLabel.FontFamily = new FontFamily("Arial");
+        }
+
+        private void resetMonth()
+        {
+            
+        }
+
+        private void loadProfitOptions(List<Product> products)
+        {
+            profitDropDown.Items.Clear();
+
+            profitDropDown.Items.Add("-");
+            foreach (Product product in products)
+            {
+                profitDropDown.Items.Add(product.Name + "   " + product.Price);
+            }
+
+            profitDropDown.SelectedIndex = 0;
+        }
+
+        private void loadCostOptions(List<Expense> expenses)
+        {
+            costDropDown.Items.Clear();
+
+            costDropDown.Items.Add("-");
+            foreach (Expense expense in expenses)
+            {
+                costDropDown.Items.Add(expense.Name + "   " + expense.Price);
+            }
+
+            costDropDown.SelectedIndex = 0;
+        }
+
+        private void addProfitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (profitDropDown.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please select a product to add to the profit list.");
+                return;
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void addCostButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (costDropDown.SelectedIndex == 0) {
+                MessageBox.Show("Please select an expense to add to the cost list.");
+                return;
+            }
+            else
+            {
+                
+            }
         }
     }
 }
